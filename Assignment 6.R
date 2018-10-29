a50a33194092db9e88bbec8fb89419e41acabd03
orjanmikal <orjanantonsen@hotmail.com>
2018-10-29 15:15:27 +0100

    Fixed some task I didnt complete

--- Assignment 6.R
+++ Assignment 6.R
@@ -1,7 +1,8 @@
 library(tibble)
-library(gapminder)
+library(data.table)
 library(tidyverse)
 
+# Task 1
 DS = tribble(
   ~fk_account_code, ~Ansvar, ~fk_function_code, ~fk_project_code, ~amount,
   101030,40220,"Det",3432,1493.00,
@@ -12,28 +13,42 @@ DS = tribble(
   101030,40360,"Sal",NA,125534.00,
   101030,40280,"Pol",NA,251611.00)
 
+# 1
 DS$fk_project_code <- NULL
 
+# 2
+DS$Ansvar <- as.character(DS$Ansvar)
+str(DS)
+
+DS <- separate(DS, Ansvar, into = c("Ansvar1_3", "rest"), sep = 3)
+DS %>% group_by(Ansvar1_3) %>% summarise(sum=sum(amount))
+
+# 3
 DS$fk_function_code <- ordered(DS$fk_function_code,
                      levels = c("Det","Sal", "Met", "Pol"),
-                     labels = c("Supplies", "Supplies", "Inventories", "Exopenses"))
+                     labels = c("Supplies", "Supplies", "Inventories", "Other Expenses"))
 
 
 # Task 2
 
+# 1
 df <- data.frame(Product=gl(3,10,labels=c("A","B", "C")), 
                  Year=factor(rep(2002:2011,3)), 
                  Sales=1:30)
-A <- df %>%
-  filter(Product == "A") %>%
-  mutate(Sales_share = Sales/55)
 
-B <- df %>%
-  filter(Product == "B") %>%
-  mutate(Sales_share = Sales/165)
+DT <- data.table(df)
+str(DT)
+# When using data.table, choose index or key variable
+setkey(DT, "Year")
+# Calculate shares by Year
+DT <- DT[ , Share :=  Sales/sum(Sales), by=list(Year)]
 
-C <- df %>%
-  filter(Product == "C") %>%
-  mutate(Sales_share = Sales/255)
+# 2
 
-df <- rbind(A, B, C)
+library(latticeExtra)
+
+  
+xyplot(Sales +Share ~ Year, groups=Product, data = DT,
+       t="b", scales =list(relation="free", x=list(rot=45),
+       auto.key = list(space="top", column = 3), 
+       main="Product information")
